6867c97cb172b5d18d393d66825e38f1e2b19041
NaveenWebDev <ns222280@gmail.com>
2023-07-28 14:07:16 +0530

    my message completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("naveen");
+  const [chat, setChat] = useState([
+    {name:'naveen',message:'messageOneOhyaa'},
+    {name:'abhijeet',message:'messageOneOhyaa'},
+    {name:'abhijeet',message:'yup man'}
+  ])
+
+  const [msg, setMsg] = useState('');
+  const sendchat = ()=>{
+    const c = [...chat];
+    c.push({name:name,message:msg});
+    setChat(c);
+    setMsg("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+      <div>
+        <input type="text" placeholder='enter your name' onBlur={e=>setName(e.target.value)} />
+      </div>
+
+      <h2>User: {name}</h2>
+      <div className="chat-container">
+
+      { chat.map( c=><div className={`container ${c.name===name? 'me' :''}`}>
+        <p className='chatbox'>
+          <strong>{c.name}:</strong>
+          <span>{c.message}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>)}
+      
+      <div className='container'>
+        <p className='chatbox'>
+          <strong>name:</strong>
+          <span>Chat Message</span>
+        </p>
+      </div>
+
+      </div>
+      <div className='btn'>
+        <input type="text" placeholder='enter you message' value={msg} onInput={e=>setMsg(e.target.value)} />
+        <button onClick={e=>sendchat()}>Send</button>
+      </div>
     </div>
   );
 }
